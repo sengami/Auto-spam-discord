e5300298eac8aa03c2ec2ce93a2d97be985a9df7
Sengami <63180226+sengami@users.noreply.github.com>
2020-10-10 14:59:31 +0800

    324163

--- main.py
+++ main.py
@@ -4,13 +4,8 @@ from discord.ext import commands
 bot = commands.Bot(command_prefix='@@') 
 
 @bot.event()
-async def on_member_join():
-
-
-
-
-
-
+async def on_ready():
+    await print('>>ON READY<<')
 
 @bot.command()
 async def rip(ctx):
@@ -24,4 +19,4 @@ async def on_guild_channel_create(channel):
   while True:
     await channel.send("@everyone  R-OOKIE in coding", tts=True)
 
-bot.run('token')
+bot.run('NzY0Mzc5NTk3MzcwMjk0Mjcy.X4FZ-Q.m-ZNM-7upiyDxkLQKYxAQSAHabA')
